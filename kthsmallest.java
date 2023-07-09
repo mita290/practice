2f6e9b09bf897bcfe170d897078fdd0e05e8a794
manjushree <111616856+mita290@users.noreply.github.com>
2023-07-09 12:34:15 +0530

    Add files via upload

---
+++ kthsmallest.java
@@ -0,0 +1,39 @@
+import java.util.Scanner; 
+ 
+public class Main { 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in); 
+        int size = sc.nextInt(); 
+        int[] arr = new int[size]; 
+        for (int i = 0; i < size; i++) { 
+            arr[i] = sc.nextInt(); 
+        } 
+        int k = sc.nextInt(); 
+ 
+        KthSmallestElement kthSmallest = new KthSmallestElement(); 
+        int kth = kthSmallest.findKthSmallest(arr, size, k); 
+ 
+        System.out.println(kth); 
+    } 
+}
+
+class KthSmallestElement { 
+    public int findKthSmallest(int[] arr, int size, int k) { 
+        int temp=0;
+        for(int i=0; i<size-1; i++){
+            for(int j=0; j <size-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<size;i++){
+            
+            System.out.println(arr[i]);
+            
+        }
+        return arr[k-1];
+    } 
+}
